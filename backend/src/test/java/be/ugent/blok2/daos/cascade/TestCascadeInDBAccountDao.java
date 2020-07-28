030c2bc8fc1fa60a74152f797f09b9d9e541a39f
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-07-28 22:56:44 +0200

    Prepare the attribute variables for TestCascadeInDBAccountDao

--- backend/src/test/java/be/ugent/blok2/daos/cascade/TestCascadeInDBAccountDao.java
+++ backend/src/test/java/be/ugent/blok2/daos/cascade/TestCascadeInDBAccountDao.java
@@ -1,8 +1,17 @@
 package be.ugent.blok2.daos.cascade;
 
+import be.ugent.blok2.TestSharedMethods;
+import be.ugent.blok2.daos.*;
+import be.ugent.blok2.model.penalty.Penalty;
+import be.ugent.blok2.model.penalty.PenaltyEvent;
+import be.ugent.blok2.model.reservables.Location;
+import be.ugent.blok2.model.reservations.LocationReservation;
+import be.ugent.blok2.model.reservations.LockerReservation;
+import be.ugent.blok2.model.users.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,9 +21,54 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles({"db", "test"})
 public class TestCascadeInDBAccountDao {
 
+    @Autowired
+    private IAccountDao accountDao;
+
+    @Autowired
+    private ILocationDao locationDao;
+
+    @Autowired
+    private ILocationReservationDao locationReservationDao;
+
+    @Autowired
+    private ILockerReservationDao lockerReservationDao;
+
+    @Autowired
+    private IPenaltyEventsDao penaltyEventsDao;
+
+    @Autowired
+    private IScannerLocationDao scannerLocationDao;
+
+    // this will be the test user
+    private User testUser;
+
+    // for cascade on SCANNERS_LOCATION, LOCATION_RESERVATIONS
+    // and LOCKER_RESERVATIONS, a Location must be available
+    private Location testLocation1;
+    private Location testLocation2;
+
+    // to test cascade on LOCATION_RESERVATIONS
+    private LocationReservation testLocationReservation1;
+    private LocationReservation testLocationReservation2;
+
+    // to test cascade on LOCKER_RESERVATIONS
+    private LockerReservation testLockerReservation1;
+    private LockerReservation testLockerReservation2;
+
+    // to test cascade on PENALTY_BOOK
+    private PenaltyEvent testPenaltyEvent;
+    private Penalty testPenalty1;
+    private Penalty testPenalty2;
+
     @Before
     public void setup() {
         // Use test database
+        TestSharedMethods.setupTestDaoDatabaseCredentials(accountDao);
+        TestSharedMethods.setupTestDaoDatabaseCredentials(locationDao);
+        TestSharedMethods.setupTestDaoDatabaseCredentials(locationReservationDao);
+        TestSharedMethods.setupTestDaoDatabaseCredentials(lockerReservationDao);
+        TestSharedMethods.setupTestDaoDatabaseCredentials(penaltyEventsDao);
+        TestSharedMethods.setupTestDaoDatabaseCredentials(scannerLocationDao);
 
         // Setup test objects
 
@@ -27,7 +81,12 @@ public class TestCascadeInDBAccountDao {
         // Remove test objects from database
 
         // Use regular database
-
+        accountDao.useDefaultDatabaseConnection();
+        locationDao.useDefaultDatabaseConnection();
+        locationReservationDao.useDefaultDatabaseConnection();
+        lockerReservationDao.useDefaultDatabaseConnection();
+        penaltyEventsDao.useDefaultDatabaseConnection();
+        scannerLocationDao.useDefaultDatabaseConnection();
     }
 
 }
