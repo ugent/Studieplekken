08199e22f3c7175d605bb5c2f9c253c4921c4c32
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-08-01 17:07:51 +0200

    Provide general layout: header, router-outlet, footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,45 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  supportedLanguages = ['nl', 'en'];
+  currentLanguageIndex: number;
 
-  constructor(private translate: TranslateService) {
+  // every navigationItem should have an entry in assets/i18n/*.json
+  navigationItems: string[] = ['dashboard', 'profile', 'scan', 'management', 'information'];
+
+  constructor(private translate: TranslateService,
+              private router: Router) {
     // setup translation
-    const lang = translate.getBrowserLang();
-    translate.use(lang.match(/en|nl/) ? lang : 'nl');
+    this.setupLanguage();
   }
 
-  getKeys(obj: {}): string[] {
-    return Object.keys(obj);
+  setupLanguage(): void {
+    this.currentLanguageIndex = this.supportedLanguages.indexOf(this.translate.getBrowserLang());
+    if (this.currentLanguageIndex < 0) {
+      this.currentLanguageIndex = 0;
+    }
+    this.translate.use(this.supportedLanguages[this.currentLanguageIndex]);
+  }
+
+  currentLanguage(): string {
+    return this.supportedLanguages[this.currentLanguageIndex];
+  }
+
+  otherSupportedLanguage(): string {
+    return this.supportedLanguages[(this.currentLanguageIndex + 1) % this.supportedLanguages.length];
+  }
+
+  changeLanguage(): void {
+    this.currentLanguageIndex = (this.currentLanguageIndex + 1) % this.supportedLanguages.length;
+    this.translate.use(this.supportedLanguages[this.currentLanguageIndex]);
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
+
+  // TODO
+  isLoggedIn(): boolean {
+    return false;
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,20 @@ import { AppComponent } from './app.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {RouterModule, Routes} from '@angular/router';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
 }
+
+const routes: Routes = [
+  {path: 'dashboard', component: AppComponent},
+  {path: 'profile', component: AppComponent},
+  {path: 'scan', component: AppComponent},
+  {path: 'management', component: AppComponent},
+  {path: 'information', component: AppComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +28,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
     TranslateModule.forRoot({
       defaultLanguage: 'nl',
