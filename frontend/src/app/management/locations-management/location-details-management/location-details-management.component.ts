3d6e22b6442abc2563dba73903a0c208f41b1cf7
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-08-17 21:57:15 +0200

    Clean: Location details -> collapsable panel views for (1) details, (2) calendar for opening days, (3) calendar for locker reservations

---
+++ frontend/src/app/management/locations-management/location-details-management/location-details-management.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Location} from '../../../shared/model/Location';
+import {LocationService} from '../../../services/api/locations/location.service';
+import {CalendarEvent} from 'angular-calendar';
+import {
+  CalendarPeriodConstructor,
+  CalendarPeriod
+} from '../../../shared/model/CalendarDay';
+
+@Component({
+  selector: 'app-location-details-management',
+  templateUrl: './location-details-management.component.html',
+  styleUrls: ['./location-details-management.component.css']
+})
+export class LocationDetailsManagementComponent implements OnInit {
+  locationObs: Observable<Location>;
+
+  Object = Object;
+
+  constructor(private locationService: LocationService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const locationName = this.route.snapshot.paramMap.get('locationName');
+    this.locationObs = this.locationService.getLocation(locationName);
+  }
+}
