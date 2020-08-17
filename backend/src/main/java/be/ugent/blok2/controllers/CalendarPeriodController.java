d3167717f6072b3361a2d07ff8386480b98f3692
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-08-17 22:00:38 +0200

    Prepare controller for calendar periods

--- backend/src/main/java/be/ugent/blok2/controllers/CalendarPeriodController.java
+++ backend/src/main/java/be/ugent/blok2/controllers/CalendarPeriodController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/locations/calendar")
 public class CalendarPeriodController {
+
+    private final Logger logger = Logger.getLogger(CalendarPeriodController.class.getSimpleName());
+
     private final ICalendarPeriodDao calendarPeriodDao;
 
     @Autowired
@@ -27,12 +31,25 @@ public class CalendarPeriodController {
         }
     }
 
+    @PostMapping
+    public void addCalendarPeriods(@RequestBody List<CalendarPeriod> calendarPeriods) throws SQLException {
+        this.calendarPeriodDao.addCalendarPeriods(calendarPeriods);
+    }
+
     @PutMapping
-    public void updateCalendarPeriods(@RequestBody List<CalendarPeriod> from,
-                                      @RequestBody List<CalendarPeriod> to) {
+    public void updateCalendarPeriods(@RequestBody List<CalendarPeriod>[] fromAndTo) {
+        List<CalendarPeriod> from = fromAndTo[0];
+        List<CalendarPeriod> to = fromAndTo[1];
+
+        logger.info("updateCalendarPeriods, from size = " + from.size() + " and to size = " + to.size());
         // TODO: analyze the differences between from and to. Based on the analysis, call
         //   - calendarPeriodDao.addCalendarPeriods(...)
         //   - calendarPeriodDao.updateCalendarPeriods(..., ...)
         //   - calendarPeriodDao.deleteCalendarPeriods(...)
+    }
+
+    @DeleteMapping
+    public void deleteCalendarPeriods(@RequestBody List<CalendarPeriod> calendarPeriods) throws SQLException {
+        this.calendarPeriodDao.deleteCalendarPeriods(calendarPeriods);
     }
 }
