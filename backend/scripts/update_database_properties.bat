12be132370347d3278c98b3f033ad7945e1ea910
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-09-07 14:27:34 +0200

    Edit the application_queries_formatting.pl script, and update bat/sh file for updating the database.properties file

--- backend/scripts/update_database_properties.bat
+++ backend/scripts/update_database_properties.bat
@@ -1,5 +1,6 @@
 cd ../database
 
-perl application_queries_formatting.pl application_queries.sql > a
-perl -ne "s/\s+$//; print \"$_\n\";" a > database_onlyQuerries.properties
-DEL a
+perl application_queries_formatting.pl application_queries.sql ../src/main/resources/database.properties > out
+perl -e "while (<>) { print; }" out > ../src/main/resources/database.properties
+
+DEL out
