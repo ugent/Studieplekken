05698b5be752e8046abd99530ce796702587a347
mcbloch <maxime.bloch@ugent.be>
2020-09-18 11:53:03 +0200

    Move ldap config to spring-boot application properties.

--- backend/src/main/java/blok2/security/LdapConfig.java
+++ backend/src/main/java/blok2/security/LdapConfig.java
@@ -1,21 +1,28 @@
 package blok2.security;
 
 import blok2.helpers.Resources;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 
 @Configuration
+@ConfigurationProperties(prefix = "ldap")
 public class LdapConfig {
+
+    private String url;
+    private String base;
+    private String userDn;
+    private String password;
 
     @Bean
     public LdapContextSource ldapContextSource() {
         LdapContextSource ldapContextSource = new LdapContextSource();
-        ldapContextSource.setUrl(Resources.blokatugentConf.getString("ldap.url"));
-        ldapContextSource.setBase(Resources.blokatugentConf.getString("ldap.base"));
-        ldapContextSource.setUserDn(Resources.blokatugentConf.getString("ldap.userDn"));
-        ldapContextSource.setPassword(Resources.blokatugentConf.getString("ldap.password"));
+        ldapContextSource.setUrl(url);
+        ldapContextSource.setBase(base);
+        ldapContextSource.setUserDn(userDn);
+        ldapContextSource.setPassword(password);
         return ldapContextSource;
     }
 
@@ -24,4 +31,19 @@ public class LdapConfig {
         return new LdapTemplate(ldapContextSource());
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public void setUserDn(String userDn) {
+        this.userDn = userDn;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
