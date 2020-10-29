694595c9daaa64178295553f477c1d3191497c58
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-10-29 08:39:28 +0100

    Put configuration in environment aware conf files

--- backend/src/main/java/blok2/security/handlers/AuthSuccessHandler.java
+++ backend/src/main/java/blok2/security/handlers/AuthSuccessHandler.java
@@ -1,20 +1,21 @@
 package blok2.security.handlers;
 
-import blok2.helpers.Resources;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserCache;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Service
+@Configuration
+@ConfigurationProperties(prefix = "redirect")
 public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+
+    private String successLoginRedirectUrl;
 
     /**
      * When the user has successfully logged in, we need to redirect the user to the
@@ -24,7 +25,11 @@ public class AuthSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException {
-        httpServletResponse.sendRedirect(Resources.blokatugentConf.getString("successfulLoginRedirectUrl"));
+        httpServletResponse.sendRedirect(successLoginRedirectUrl);
+    }
+
+    public void setSuccessLoginRedirectUrl(String successLoginRedirectUrl) {
+        this.successLoginRedirectUrl = successLoginRedirectUrl;
     }
 
 }
