f009284a1e89825fba7f1e0e53394732f4d667c0
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-11-01 15:09:29 +0100

    Configuration class to enable PreAuthorize annotation

---
+++ backend/src/main/java/blok2/security/config/MethodSecuritySecurity.java
@@ -0,0 +1,12 @@
+package blok2.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true
+)
+public class MethodSecuritySecurity extends GlobalMethodSecurityConfiguration {
+}
