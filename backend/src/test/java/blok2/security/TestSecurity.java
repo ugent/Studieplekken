fa4b1ca8e8aad4a7911c32a8dcd6196cbc644435
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-11-01 19:34:14 +0100

    To be completed: autorisatie testen

---
+++ backend/src/test/java/blok2/security/TestSecurity.java
@@ -0,0 +1,37 @@
+package blok2.security;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class TestSecurity {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    protected MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilter(springSecurityFilterChain)
+                .build();
+    }
+}
