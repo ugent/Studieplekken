cc93de96d5631d26e21f9b8b3a86779992fa6a23
tverum <tim.vanerum@ugent.be>
2020-11-04 14:47:29 +0100

    Seed vergeten mergen Changes to be committed: 	modified:   ../database/seed.sql

--- backend/database/seed.sql
+++ backend/database/seed.sql
@@ -28,24 +28,14 @@ DECLARE auth_id integer;
 DECLARE build_id_therminal integer;
 
 BEGIN
-<<<<<<< HEAD
   INSERT INTO public.authority (authority_name, description) values ('DSA', 'Dienst StudentenActiviteiten') RETURNING authority_id into auth_id;
   INSERT INTO public.buildings (building_name, address) VALUES ('Therminal', 'Hoveniersberg 24, 9000 Gent') RETURNING building_id into build_id_therminal;
-  INSERT INTO public.locations (name, building_id, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english)
-    VALUES  ('Turbinezaal', build_id_therminal, 50, 100, '', auth_id, 'neder', 'engl'),
-                ('Plenaire vergaderzaal', build_id_therminal, 30, 0, '', auth_id, '', ''),
-                ('Podiumzaal', build_id_therminal, 100, 0, '', auth_id, '', ''),
-                ('Trechterzaal', build_id_therminal, 80, 0, '', auth_id, '', '');
+  INSERT INTO public.locations (name, building_id, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english, forGroup)
+    VALUES  ('Turbinezaal', build_id_therminal, 50, 100, '', auth_id, 'neder', 'engl', false),
+                ('Plenaire vergaderzaal', build_id_therminal, 30, 0, '', auth_id, '', '', true),
+                ('Podiumzaal', build_id_therminal, 100, 0, '', auth_id, '', '', false),
+                ('Trechterzaal', build_id_therminal, 80, 0, '', auth_id, '', '', false);
   INSERT INTO public.roles_user_authority (user_id, authority_id) VALUES ('002', auth_id);
-=======
-  INSERT INTO public.authority (authority_name, description) values ('DSA', 'Dienst StudentenActiviteiten') RETURNING authority_id into tableId;
-  INSERT INTO public.locations (name, address, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english, forGroup)
-    VALUES  ('Turbinezaal', 'Hoveniersberg 24, 9000 Gent', 50, 100, '', tableId, 'neder', 'engl', false),
-                ('Plenaire vergaderzaal', 'Hoveniersberg 24, 9000 Gent', 30, 0, '', tableId, '', '', true),
-                ('Podiumzaal', 'Hoveniersberg 24, 9000 Gent', 100, 0, '', tableId, '', '', false),
-                ('Trechterzaal', 'Hoveniersberg 24, 9000 Gent', 80, 0, '', tableId, '', '', false);
-  INSERT INTO public.roles_user_authority (user_id, authority_id) VALUES ('002',tableId);
->>>>>>> master
 END $$;
 
 
@@ -56,23 +46,14 @@ DECLARE build_id_sterreS9 integer;
 
 BEGIN
   INSERT INTO authority (authority_name, description) VALUES ('WE', 'Faculteit wetenschappen') RETURNING authority_id into new_authority_id;
-<<<<<<< HEAD
   INSERT INTO public.buildings (building_name, address) VALUES ('Sterre S5', 'Krijgslaan 281, 9000 Gent') RETURNING building_id into build_id_sterreS5;
   INSERT INTO public.buildings (building_name, address) VALUES ('Sterre S9', 'Krijgslaan 281, 9000 Gent') RETURNING building_id into build_id_sterreS9;
-  INSERT INTO locations (name, building_id, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english)
-    VALUES  ('Sterre S9, PC lokaal 3rd verdiep', build_id_sterreS9, 40, 100, '', new_authority_id, 'Klaslokaal met computers', 'Classroom with computers'),
+  INSERT INTO locations (name, building_id, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english, forGroup)
+    VALUES  ('Sterre S9, PC lokaal 3rd verdiep', build_id_sterreS9, 40, 100, '', new_authority_id, 'Klaslokaal met computers', 'Classroom with computers', false),
             ('Sterre S5, Bib', build_id_sterreS5, 100, 100, '', new_authority_id,
                     'Informatie over de bib kan hier gevonden worden: https://lib.ugent.be/nl/libraries/WEBIB.',
-                    'Information about the bib itself can be found here: https://lib.ugent.be/nl/libraries/WEBIB.'),
-            ('Sterre S5, Eetzaal', build_id_sterreS5, 130, 100, '', new_authority_id, '', '');
-=======
-  INSERT INTO locations (name, address, number_of_seats, number_of_lockers, image_url, authority_id, description_dutch, description_english, forGroup)
-    VALUES  ('Sterre S9, PC lokaal 3rd verdiep', 'Krijgslaan 281, 9000 Gent', 40, 100, '', new_authority_id, 'Klaslokaal met computers', 'Classroom with computers', false),
-            ('Sterre S5, Bib', 'Krijgslaan 281, 9000 Gent', 100, 100, '', new_authority_id,
-                    'Informatie over de bib kan hier gevonden worden: https://lib.ugent.be/nl/libraries/WEBIB.',
                     'Information about the bib itself can be found here: https://lib.ugent.be/nl/libraries/WEBIB.', false),
-            ('Sterre S5, Eetzaal', 'Krijgslaan 281, 9000 Gent', 130, 100, '', new_authority_id, '', '', false);
->>>>>>> master
+            ('Sterre S5, Eetzaal', build_id_sterreS5, 130, 100, '', new_authority_id, '', '', false);
   INSERT INTO roles_user_authority (user_id, authority_id) VALUES ('002', new_authority_id);
 END $$;
 /*
