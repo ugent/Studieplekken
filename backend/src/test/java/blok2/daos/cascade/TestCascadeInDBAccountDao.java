ce94b9f3e3e72d3f873c6a8856a18ce99b40807d
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-11-05 17:22:07 +0100

    Remove manual cascade from DBAccountDao

--- backend/src/test/java/blok2/daos/cascade/TestCascadeInDBAccountDao.java
+++ backend/src/test/java/blok2/daos/cascade/TestCascadeInDBAccountDao.java
@@ -44,9 +44,6 @@ public class TestCascadeInDBAccountDao extends TestDao {
     // this will be the test user
     private User testUser;
 
-    //to connect a location to an authority
-    private Authority authority;
-
     // for cascade on SCANNERS_LOCATION, LOCATION_RESERVATIONS
     // and LOCKER_RESERVATIONS, a Location must be available
     private Location testLocation1;
@@ -60,8 +57,6 @@ public class TestCascadeInDBAccountDao extends TestDao {
     private LockerReservation testLockerReservation1;
     private LockerReservation testLockerReservation2;
 
-    // to test cascade on PENALTY_BOOK
-    private PenaltyEvent testPenaltyEvent;
     private Penalty testPenalty1;
     private Penalty testPenalty2;
 
@@ -69,7 +64,8 @@ public class TestCascadeInDBAccountDao extends TestDao {
     public void populateDatabase() throws SQLException {
         // Setup test objects
         testUser = TestSharedMethods.studentTestUser();
-        authority = TestSharedMethods.insertTestAuthority(authorityDao);
+
+        Authority authority = TestSharedMethods.insertTestAuthority(authorityDao);
         testLocation1 = TestSharedMethods.testLocation(authority.clone());
         testLocation2 = TestSharedMethods.testLocation2(authority.clone());
 
@@ -85,12 +81,12 @@ public class TestCascadeInDBAccountDao extends TestDao {
         Map<Language, String> descriptions = new HashMap<>();
         descriptions.put(Language.DUTCH, "Dit is een test omschrijving van een penalty event met code 0");
         descriptions.put(Language.ENGLISH, "This is a test description of a penalty event with code 0");
-        testPenaltyEvent = new PenaltyEvent(0, 10, descriptions);
 
         // Note: the received amount of points are 10 and 20, not testPenaltyEvent.getCode()
         // because when the penalties are retrieved from the penaltyEventDao, the list will
         // be sorted by received points before asserting, if they would be equal we can't sort
         // on the points and be sure about the equality of the actual and expected list.
+        PenaltyEvent testPenaltyEvent = new PenaltyEvent(0, 10, descriptions);
         testPenalty1 = new Penalty(testUser.getAugentID(), testPenaltyEvent.getCode(), CustomDate.now(), CustomDate.now(), testLocation1.getName(), 10, "First test penalty");
         testPenalty2 = new Penalty(testUser.getAugentID(), testPenaltyEvent.getCode(), new CustomDate(1970, 1, 1), CustomDate.now(), testLocation2.getName(), 20, "Second test penalty");
 
