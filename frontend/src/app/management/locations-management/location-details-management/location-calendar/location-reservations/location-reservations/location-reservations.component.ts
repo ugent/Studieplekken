79a28bbd7d0b6a6b10d9feebc5b054c5a3f32a83
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-11-19 22:56:32 +0100

    cherry picking bram's commit

--- frontend/src/app/management/locations-management/location-details-management/location-calendar/location-reservations/location-reservations/location-reservations.component.ts
+++ frontend/src/app/management/locations-management/location-details-management/location-calendar/location-reservations/location-reservations/location-reservations.component.ts
@@ -1,13 +1,10 @@
 import {Component, EventEmitter, Input, OnInit, Output, TemplateRef} from '@angular/core';
 import {LocationReservation} from '../../../../../../shared/model/LocationReservation';
-import {Timeslot, timeslotStartHour} from '../../../../../../shared/model/Timeslot';
+import {Timeslot} from '../../../../../../shared/model/Timeslot';
 import * as moment from 'moment';
 import {CalendarPeriod} from '../../../../../../shared/model/CalendarPeriod';
-import {map, tap} from 'rxjs/operators';
 import {LocationReservationsService} from '../../../../../../services/api/location-reservations/location-reservations.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { CalendarPeriodsService } from 'src/app/services/api/calendar-periods/calendar-periods.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-location-reservations',
@@ -24,11 +21,60 @@ export class LocationReservationsComponent implements OnInit {
   locationReservationToDelete: LocationReservation = undefined;
 
   successDeletingLocationReservation: boolean = undefined;
+  successFinishingScan: boolean = undefined;
+
+  startedScanning = false;
+
+  scannedLocationReservations: LocationReservation[];
 
   constructor(private locationReservationService: LocationReservationsService,
               private modalService: BsModalService) { }
 
   ngOnInit(): void {
+  }
+
+  // /***************
+  // *   SCANNING   *
+  // ****************/
+
+  startScanning(): void {
+    this.scannedLocationReservations = [];
+    this.startedScanning = true;
+  }
+
+  prepareFinishScanning(): void {
+    this.successFinishingScan = undefined;
+  }
+
+  finishScanning(): void {
+    this.successFinishingScan = null;
+    this.locationReservationService.scanLocationReservations(this.scannedLocationReservations)
+      .subscribe(
+        () => {
+          this.successFinishingScan = true;
+          this.updateLocationReservations();
+        }, () => {
+          this.successFinishingScan = false;
+        }
+      );
+    this.startedScanning = false;
+  }
+
+  scanLocationReservation(reservation: LocationReservation, attended: boolean): void {
+    console.log(reservation);
+    const idx = this.scannedLocationReservations.findIndex(
+      r => {
+        return r.timeslot.timeslotSeqnr === reservation.timeslot.timeslotSeqnr &&
+          r.user === reservation.user;
+      });
+
+    if (idx < 0) {
+      const lr = LocationReservation.fromJSON(reservation);
+      lr.attended = attended;
+      this.scannedLocationReservations.push(lr);
+    } else {
+      this.scannedLocationReservations[idx].attended = attended;
+    }
   }
 
   // /*************
@@ -56,15 +102,73 @@ export class LocationReservationsComponent implements OnInit {
   // *   AUXILIARIES   *
   // *******************/
 
-  onCheckboxToggle(reservation, checked): void {
-    this.locationReservationService.postLocationReservationAttendance(reservation, checked)
-      .pipe(tap(() => this.reservationChange.emit(null)))
-      .subscribe();
+  getCorrectI18NObject(reservation: LocationReservation): string {
+    if (this.isTimeslotInPast()) {
+      if (reservation.attended === null) {
+        return 'management.locationDetails.calendar.reservations.table.notScanned';
+      } else {
+        return reservation.attended ? 'general.yes' : 'general.no';
+      }
+    } else if (this.isNowWithinTimeslot()) {
+      if (reservation.attended === null) {
+        return 'management.locationDetails.calendar.reservations.table.yetToScan';
+      } else {
+        return reservation.attended ? 'general.yes' : 'general.no';
+      }
+    } else {
+      return 'general.notAvailableAbbreviation';
+    }
   }
 
-  showCheckbox(): boolean {
-    return this.currentCalendarPeriod && this.currentTimeSlot
-      && timeslotStartHour(this.currentCalendarPeriod, this.currentTimeSlot).isBefore(moment());
+  isTimeslotInPast(): boolean {
+    const timeslotDate = this.currentTimeSlot.timeslotDate.format('YYYY-MM-DD');
+    const timeslotClose = this.currentCalendarPeriod.openingTime.clone()
+      .add(this.currentCalendarPeriod.reservableTimeslotSize * (this.currentTimeSlot.timeslotSeqnr + 1), 'minutes')
+      .format('HH:mm:ss');
+
+    const timeslotTimestamp = moment(timeslotDate + ' ' + timeslotClose, 'YYYY-MM-DD HH:mm:ss');
+    return timeslotTimestamp.isBefore(moment());
+  }
+
+  isNowWithinTimeslot(): boolean {
+    const timeslotDate = this.currentTimeSlot.timeslotDate.format('YYYY-MM-DD');
+    const timeslotOpen = this.currentCalendarPeriod.openingTime.clone()
+      .add(this.currentCalendarPeriod.reservableTimeslotSize * this.currentTimeSlot.timeslotSeqnr, 'minutes')
+      .format('HH:mm:ss');
+    const timeslotClose = this.currentCalendarPeriod.openingTime.clone()
+      .add(this.currentCalendarPeriod.reservableTimeslotSize * (this.currentTimeSlot.timeslotSeqnr + 1), 'minutes')
+      .format('HH:mm:ss');
+
+    const start = moment(timeslotDate + ' ' + timeslotOpen, 'YYYY-MM-DD HH:mm:ss');
+    const end = moment(timeslotDate + ' ' + timeslotClose, 'YYYY-MM-DD HH:mm:ss');
+
+    return start.isBefore(moment()) && end.isAfter(moment());
+  }
+
+  isButtonDisabled(reservation: LocationReservation, attended: boolean): boolean {
+    for (const l of this.scannedLocationReservations) {
+      if (l.user === reservation.user && l.timeslot.timeslotSeqnr === reservation.timeslot.timeslotSeqnr &&
+          l.attended === attended) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Update this.locationReservations based on this.scannedLocationReservations
+   */
+  updateLocationReservations(): void {
+    this.scannedLocationReservations.forEach(
+      slr => {
+        const idx = this.locationReservations.findIndex(lr => lr.user === slr.user
+          && lr.timeslot.timeslotSeqnr === slr.timeslot.timeslotSeqnr);
+
+        if (idx >= 0) {
+          this.locationReservations[idx].attended = slr.attended;
+        }
+      }
+    );
   }
 
   closeModal(): void {
