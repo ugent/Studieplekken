a54c2e896556bd3c8b26ab47135909f7a1b4a35f
Maxiem Geldhof <maxiem.geldhof@ugent.be>
2020-11-19 21:36:35 +0100

    test broken

--- backend/src/test/java/Mail.java
+++ backend/src/test/java/Mail.java
@@ -1,10 +1,7 @@
 import blok2.Blok2Application;
 import blok2.controllers.BuildingController;
 import blok2.controllers.LocationController;
-import blok2.daos.IAuthorityDao;
-import blok2.daos.IBuildingDao;
-import blok2.daos.ICalendarPeriodDao;
-import blok2.daos.TestSharedMethods;
+import blok2.daos.*;
 import blok2.helpers.EmailService;
 import blok2.model.Authority;
 import blok2.model.Building;
@@ -47,7 +44,7 @@ public class Mail {
     @Autowired
     private IBuildingDao buildingDao;
     @Autowired
-    private LocationController locationDao;
+    private ILocationDao locationDao;
     @Autowired
     private ICalendarPeriodDao calendarPeriodDao;
 
