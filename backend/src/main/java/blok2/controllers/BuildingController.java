d2891dab88aac4658d8ad28738b3858e1e9d8fcb
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-11-22 20:10:23 +0100

    Make getters for buildings public

--- backend/src/main/java/blok2/controllers/BuildingController.java
+++ backend/src/main/java/blok2/controllers/BuildingController.java
@@ -30,7 +30,6 @@ public class BuildingController {
     // *************************************/
 
     @GetMapping
-    @PreAuthorize("hasAuthority('HAS_AUTHORITIES') or hasAuthority('ADMIN')")
     public List<Building> getAllBuildings() {
         try {
             return buildingDao.getAllBuildings();
@@ -42,7 +41,6 @@ public class BuildingController {
     }
 
     @GetMapping("/{buildingId}")
-    @PreAuthorize("hasAuthority('HAS_AUTHORITIES') or hasAuthority('ADMIN')")
     public Building getBuilding(@PathVariable int buildingId) {
         try {
             return buildingDao.getBuildingById(buildingId);
