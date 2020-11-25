132da66def57c91fba6a705f5a194f6edb9bc25d
mcbloch <Maxime.Bloch@UGent.be>
2020-11-25 17:17:35 +0000

    Revert "Fix updating seats"

--- backend/src/main/java/blok2/helpers/authorization/AuthorizedController.java
+++ backend/src/main/java/blok2/helpers/authorization/AuthorizedController.java
@@ -5,14 +5,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.AccessControlException;
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 public abstract class AuthorizedController {
+
+
 
     public <T> void isAuthorized(BiFunction<T, User, Boolean> p, T entity) {
         isAuthorized(p, entity, "You do not have the necessary permissions.");
     }
-
     public <T> void isAuthorized(BiFunction<T, User, Boolean> p, T entity, String message) {
+
         Object userO = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(userO instanceof User && ((User)userO).isAdmin()) {
             // Blanket admin permissions. You're authorized.
