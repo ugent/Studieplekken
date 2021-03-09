8793aa0f2c0679a320ab190568376fcad5975497
Bram Van de Walle <bram.vandewalle@ugent.be>
2021-03-09 21:47:44 +0100

    Finish frontend

--- frontend/src/app/miscellaneous/opening-hours-overview/opening-hours-overview.component.ts
+++ frontend/src/app/miscellaneous/opening-hours-overview/opening-hours-overview.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable, Subject} from 'rxjs';
 import {LocationService} from '../../services/api/locations/location.service';
-import {catchError} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 import {of} from 'rxjs/internal/observable/of';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-opening-hours-overview',
@@ -37,6 +38,46 @@ export class OpeningHoursOverviewComponent implements OnInit {
 
   stringOrPlaceholder(str: string): string {
     return str === null || str === undefined || str.length === 0 ? '-' : str;
+  }
+
+  mondayDate(year: number, week: number): string {
+    const mondayDate = moment().set('year', year).isoWeek(week).startOf('isoWeek');
+    return mondayDate.format('DD/MM/YYYY');
+  }
+
+  sundayDate(year: number, week: number): string {
+    const sundayDate = moment().set('year', year).isoWeek(week).endOf('isoWeek');
+    return sundayDate.format('DD/MM/YYYY');
+  }
+
+  previousWeek(): void {
+    this.overviewObs = null;
+    const year = this.weekNr === 1 ? this.year - 1 : this.year;
+    const week = this.weekNr === 1 ? 52 : this.weekNr - 1;
+    this.retrieveOverview(year, week);
+  }
+
+  nextWeek(): void {
+    this.overviewObs = null;
+    const year = this.weekNr === 52 ? this.year + 1 : this.year;
+    const week = this.weekNr === 52 ? 1 : this.weekNr + 1;
+    this.retrieveOverview(year, week);
+  }
+
+  retrieveOverview(year: number, week: number): void {
+    this.overviewObs = this.locationService.getOpeningOverviewOfWeek(year, week)
+      .pipe(
+        tap(
+          () => {
+            this.year = year;
+            this.weekNr = week;
+          }
+        )
+      );
+  }
+
+  isInPast(year: number, week: number): boolean {
+    return moment().set('year', year).isoWeek(week).isBefore(moment());
   }
 
 }
