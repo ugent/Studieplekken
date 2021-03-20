33d2b42fdf24709e93fa36eea3792bd45ea0afdb
Bram Van de Walle <bram.vandewalle@ugent.be>
2021-03-20 21:42:13 +0100

    Check API response before changing attendance of object

--- frontend/src/app/services/barcode.service.ts
+++ frontend/src/app/services/barcode.service.ts
@@ -8,25 +8,25 @@ export class BarcodeService {
 
   constructor() { }
 
+  private static fromEAN13(code: string): string {
+    return code.substring(0, code.length - 1);
+  }
+
+  private static fromCODE128(code: string): string {
+    return code;
+  }
+
+  private static fromUPCA(code: string): string {
+    return '0' + code.substring(0, code.length - 1);
+  }
+
   public isValid(rlist: LocationReservation[], code: string): boolean {
-    return !!this.getReservation(rlist, code)
+    return !!this.getReservation(rlist, code);
   }
 
   public getReservation(rlist: LocationReservation[], code: string): LocationReservation {
-    const match = (f, r) => r.user.augentID === f(code)
-    return rlist.find(r => match(this.fromEAN13, r) || match(this.fromCODE128, r) || match(this.fromUPCA, r))  || null
-  }
-
-
-  private fromEAN13(code: string) {
-    return code.substring(0, code.length-1)
-  }
-
-  private fromCODE128(code: string) {
-    return code
-  }
-
-  private fromUPCA(code: string) {
-    return "0" + code.substring(0, code.length - 1)
+    const match = (f, r) => r.user.augentID === f(code);
+    return rlist.find(r => match(BarcodeService.fromEAN13, r) || match(BarcodeService.fromCODE128, r)
+      || match(BarcodeService.fromUPCA, r)) || null;
   }
 }
