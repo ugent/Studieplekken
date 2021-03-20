976a0477bdc3694a2bec65f2488a09fce859f312
mcbloch <maxime.bloch@ugent.be>
2021-03-20 17:27:56 +0100

    Remove logs, add translation

--- frontend/src/app/shared/inputs/moment-timeslot-size/moment-timeslot-size.component.ts
+++ frontend/src/app/shared/inputs/moment-timeslot-size/moment-timeslot-size.component.ts
@@ -1,14 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
-import * as moment from 'moment';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import 'moment-duration-format';
 
 @Component({
   selector: 'app-moment-timeslot-size',
   templateUrl: './moment-timeslot-size.component.html',
-  styleUrls: ['./moment-timeslot-size.component.css']
+  styleUrls: ['./moment-timeslot-size.component.css'],
 })
 export class MomentTimeslotSizeComponent implements OnInit, OnChanges {
-
   /**
    * This is the value that will eventually be emitted
    */
@@ -32,7 +30,8 @@ export class MomentTimeslotSizeComponent implements OnInit, OnChanges {
    */
   modelAsString: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -41,23 +40,21 @@ export class MomentTimeslotSizeComponent implements OnInit, OnChanges {
    * This is called whenever the value is changed by the application (e.g. loaded from server)
    */
   ngOnChanges(changes): void {
-    console.log(changes)
-
-    if (this.periodDuration) {
-      console.log(this.periodDuration)
-      this.modelAsString = `${Math.abs(Math.round(this.periodDuration / this.model))}`
-    } else {
-      this.modelAsString="0"
-    }
+    const newValue = this.periodDuration
+      ? Math.abs(Math.round(this.periodDuration / this.model))
+      : 0;
+    this.modelAsString = `${newValue}`;
   }
 
   /**
    * This is called whenever the user changes the value, outputValue should be emitted using timeslotSizeChange
    */
   onNewDate(): void {
-    const amountOfSlots = +this.modelAsString;
-    if(this.periodDuration) {
-      const outputValue = Math.round(Math.abs(this.periodDuration / amountOfSlots))
+    const amountOfSlots = Number(this.modelAsString);
+    if (this.periodDuration) {
+      const outputValue = Math.round(
+        Math.abs(this.periodDuration / amountOfSlots)
+      );
       this.modelChange.next(outputValue);
     }
   }
