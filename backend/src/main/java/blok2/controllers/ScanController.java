33ec326a030bd42841195ee5caacebc1a4ed8604
Bram Van de Walle <bram.vandewalle@ugent.be>
2021-04-29 11:05:36 +0200

    Remove redundant endpoints

--- backend/src/main/java/blok2/controllers/ScanController.java
+++ backend/src/main/java/blok2/controllers/ScanController.java
@@ -1,11 +1,8 @@
 package blok2.controllers;
 
 import blok2.daos.ILocationDao;
-import blok2.daos.ILocationReservationDao;
 import blok2.helpers.authorization.AuthorizedLocationController;
-import blok2.model.calendar.Timeslot;
 import blok2.model.reservables.Location;
-import blok2.model.reservations.LocationReservation;
 import blok2.model.users.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,14 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,12 +27,10 @@ public class ScanController extends AuthorizedLocationController {
     private final Logger logger = LoggerFactory.getLogger(ScanController.class);
 
     private final ILocationDao locationDao;
-    private final ILocationReservationDao reservationDao;
 
     @Autowired
-    public ScanController(ILocationDao locationDao, ILocationReservationDao reservationDao) {
+    public ScanController(ILocationDao locationDao) {
         this.locationDao = locationDao;
-        this.reservationDao = reservationDao;
     }
 
     @GetMapping("/locations")
@@ -51,22 +44,6 @@ public class ScanController extends AuthorizedLocationController {
                     user.getUserAuthorities().contains(f.getAuthority()) ||
                     user.getUserVolunteer().contains(f.getLocationId())
             ).collect(Collectors.toList());
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            logger.error(Arrays.toString(e.getStackTrace()));
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Database error");
-        }
-    }
-
-    @GetMapping("/users/{locationId}")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public List<User> getUsersToScanAtLocation(@PathVariable("locationId") int locationId) {
-        try {
-            Timeslot timeslot = locationDao.getLocationById(locationId).getCurrentTimeslot();
-            if(timeslot == null)
-                return Collections.emptyList();
-
-            return reservationDao.getAllLocationReservationsOfTimeslot(timeslot).stream().map(LocationReservation::getUser).collect(Collectors.toList());
         } catch (SQLException e) {
             logger.error(e.getMessage());
             logger.error(Arrays.toString(e.getStackTrace()));
--- frontend/src/app/services/api/scan/scanning.service.ts
+++ frontend/src/app/services/api/scan/scanning.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Location } from '../../../shared/model/Location';
 import { api } from '../endpoints';
-import { User } from '../../../shared/model/User';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +12,5 @@ export class ScanningService {
 
   getLocationsToScan(): Observable<Location[]> {
     return this.http.get<Location[]>(api.scanningLocations);
-  }
-
-  getUsersForLocationToScan(locationId: number): Observable<User[]> {
-    return this.http.get<User[]>(
-      api.usersToScanAtLocation.replace('{locationId}', String(locationId))
-    );
   }
 }
