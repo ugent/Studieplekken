18e11e94f78d4fc1045fad86070b9a51ff29f5d0
smessie <smessie@smessie.com>
2021-07-21 16:28:30 +0200

    Add button to add admin on admin management page

--- frontend/src/app/management/admins-management/admins-management.component.ts
+++ frontend/src/app/management/admins-management/admins-management.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { objectExists } from '../../shared/GeneralFunctions';
 import { Observable, of } from 'rxjs';
-import { User } from '../../shared/model/User';
+import { User, UserConstructor } from '../../shared/model/User';
 import { UserService } from '../../services/api/users/user.service';
 import { catchError, tap } from 'rxjs/operators';
 import { ApplicationTypeFunctionalityService } from '../../services/functionality/application-type/application-type-functionality.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admins-management',
@@ -13,6 +15,13 @@ import { ApplicationTypeFunctionalityService } from '../../services/functionalit
 })
 export class AdminsManagementComponent implements OnInit {
   loading = true;
+  adminsObs: Observable<User[]>;
+
+  formGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+  neverSearched: true;
   filteredUsers: Observable<User[]>;
 
   errorOnRetrievingAdmins = false; // booleanId = 0
@@ -21,12 +30,14 @@ export class AdminsManagementComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private functionalityService: ApplicationTypeFunctionalityService
-  ) {}
+    private functionalityService: ApplicationTypeFunctionalityService,
+    private modalService: BsModalService,
+  ) {
+  }
 
   ngOnInit(): void {
     this.showPenaltyPoints = this.functionalityService.showPenaltyFunctionality();
-    this.filteredUsers = this.userService.getAdmins().pipe(
+    this.adminsObs = this.userService.getAdmins().pipe(
       tap(() => (this.loading = false)),
       catchError((e) => {
         this.errorOnRetrievingAdmins = !!e;
@@ -60,5 +71,71 @@ export class AdminsManagementComponent implements OnInit {
       (objectExists(value.lastName) && value.lastName.length > 0) ||
       (objectExists(value.barcode) && value.barcode.length > 0)
     );
+  }
+
+  showAdd(template: TemplateRef<unknown>): void {
+    this.modalService.show(template);
+  }
+
+  submitSearch(value: {
+    firstName: string;
+    lastName: string;
+  }): void {
+    this.neverSearched = null;
+    if (
+      objectExists(value.firstName) &&
+      value.firstName.trim().length > 0 &&
+      objectExists(value.lastName) &&
+      value.lastName.trim().length > 0
+    ) {
+      // search by first and last name
+      this.filteredUsers = this.userService.getUsersByFirstAndLastName(
+        value.firstName,
+        value.lastName
+      );
+    } else if (
+      objectExists(value.firstName) &&
+      value.firstName.trim().length > 0
+    ) {
+      // search by first name
+      this.filteredUsers = this.userService.getUsersByFirstName(
+        value.firstName
+      );
+    } else if (
+      objectExists(value.lastName) &&
+      value.lastName.trim().length > 0
+    ) {
+      // search by last name
+      this.filteredUsers = this.userService.getUsersByLastName(value.lastName);
+    }
+  }
+
+  clearFilterInput(): void {
+    this.formGroup.setValue({
+      firstName: '',
+      lastName: '',
+    });
+    this.formGroup.enable();
+
+    this.filteredUsers = of<User[]>([]);
+  }
+
+  addAdmin(user: User): void {
+    const clone = UserConstructor.newFromObj(user);
+    clone.admin = true;
+    this.userService
+      .updateUser(user.userId, clone)
+      .subscribe(
+        () =>
+          (this.adminsObs = this.userService.getAdmins().pipe(
+            tap(() => (this.loading = false)),
+            catchError((e) => {
+              this.errorOnRetrievingAdmins = !!e;
+              return of<User[]>([]);
+            })
+          ))
+      );
+
+    this.modalService.hide();
   }
 }
