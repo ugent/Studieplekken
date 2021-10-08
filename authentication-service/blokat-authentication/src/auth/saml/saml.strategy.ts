5c1191e0df09655934d05abfae5ab7f7b7d54e84
ydndonck <yente.dendoncker@ugent.be>
2021-10-08 22:21:50 +0200

    No more copying of credential files.

--- authentication-service/blokat-authentication/src/auth/saml/saml.strategy.ts
+++ authentication-service/blokat-authentication/src/auth/saml/saml.strategy.ts
@@ -5,19 +5,21 @@ import { MetadataReader, toPassportConfig } from 'passport-saml-metadata';
 import * as fs from 'fs';
 import * as path from 'path';
 
+const AUTH_CREDENTIALS_DIR = '../../../auth/';
+
 @Injectable()
 export class SamlStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      ...readMetadata('./metadata-okta.xml'),
+      ...readMetadata(AUTH_CREDENTIALS_DIR + 'saml/metadata-okta.xml'),
       // callbackUrl: 'https://studieplekken-dev.ugent.be/api/SSO/saml',
       // issuer: 'https://studieplekken-dev.ugent.be/api/metadata/saml',
       callbackUrl: 'https://localhost:8087/api/SSO/saml',
       issuer: 'https://localhost:8087/api/metadata/saml',
-      privateKey: fs.readFileSync(path.join(__dirname, './key.pem')),
-      decryptionPvk: fs.readFileSync(path.join(__dirname, './key.pem')),
+      privateKey: fs.readFileSync(path.join(__dirname, AUTH_CREDENTIALS_DIR, 'saml/key.pem')),
+      decryptionPvk: fs.readFileSync(path.join(__dirname, AUTH_CREDENTIALS_DIR, 'saml/key.pem')),
     });
-    console.log(readMetadata('./sso-artevelde.xml'));
+    console.log(readMetadata(AUTH_CREDENTIALS_DIR +  'saml/sso-artevelde.xml'));
   }
 
   validate(first: any) {
--- authentication-service/blokat-authentication/src/main.ts
+++ authentication-service/blokat-authentication/src/main.ts
@@ -6,8 +6,8 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
      httpsOptions: {
-       cert: fs.readFileSync(path.join(__dirname, './cert.pem')),
-       key: fs.readFileSync(path.join(__dirname, './key.pem')),
+       cert: fs.readFileSync(path.join(__dirname, '../auth/cert.pem')),
+       key: fs.readFileSync(path.join(__dirname, '../auth/key.pem')),
      },
   });
   await app.listen(8087);
