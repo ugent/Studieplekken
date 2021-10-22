cf4153dedd25dcff9f31022ef10bfa79642c3f06
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-10-22 12:24:00 +0200

    Better missing fields logging

--- authentication-service/blokat-authentication/src/configModule/config.ts
+++ authentication-service/blokat-authentication/src/configModule/config.ts
@@ -34,7 +34,7 @@ const dummySamlUser: SamlUser = {
 
 export function isSamlUser(obj: any): obj is SamlUser {
   for (const key in dummySamlUser) {
-    if (!(key in obj)) {
+    if (!(key in obj) || !obj[key]) {
       return false;
     }
   }
@@ -44,7 +44,7 @@ export function isSamlUser(obj: any): obj is SamlUser {
 export function missingSamlUserFields(obj: any): string[] {
   const missingFields = [];
   for (const key in dummySamlUser) {
-    if (!(key in obj)) {
+    if (!(key in obj) || !obj[key]) {
       missingFields.push(key);
     }
   }
