dd29b87bf80e5556b735aa9180b72f98fb283543
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-11-29 12:15:42 +0100

    no longer connect to db

--- authentication-service/blokat-authentication/config/environment/environment.stag.ts
+++ authentication-service/blokat-authentication/config/environment/environment.stag.ts
@@ -106,7 +106,7 @@ export const configuration: Configuration = {
       'https://localhost:8080/login',
       'https://localhost:8087/login',
       'https://localhost:4200/login',
-      'https://bloklocaties.stad.gent/login'
+      'https://bloklocaties.stad.gent/login',
     ],
   },
 
