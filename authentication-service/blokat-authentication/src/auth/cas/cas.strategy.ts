c6866e9766c0ed7c361b531c0d5025f7c695f623
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-12-06 16:06:27 +0100

    rewrite of the cas strategy: the library we used was not safe

--- authentication-service/blokat-authentication/src/auth/cas/cas.strategy.ts
+++ authentication-service/blokat-authentication/src/auth/cas/cas.strategy.ts
@@ -1,45 +1,118 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-cas';
-import { Institution, SamlUser } from 'src/configModule/config';
-import { ConfigService } from 'src/configModule/config.service';
+import { Request } from "express";
+import got from "got";
+import { Strategy } from "passport-strategy";
+import { URL } from "url";
+import { XMLParser } from "fast-xml-parser";
+import { Logger } from "@nestjs/common";
 
-@Injectable()
-export class CasStrategy extends PassportStrategy(Strategy, 'cas') {
-  constructor(private configService: ConfigService) {
-    super({
-      version: 'CAS3.0',
-      ssoBaseURL: 'https://login.ugent.be',
-      serverBaseURL:
-        configService.getCurrentConfiguration().auth.cas.serverBaseURL,
-      validateURL: '/serviceValidate',
-      useSaml: false,
+export interface CasStrategyOptions {
+  ssoBaseURL: string;
+  ssoLoginPath: string;
+  ssoValidationPath: string;
+  serverBaseURL: string;
+}
+
+export class CasStrategy extends Strategy {
+  constructor(private options: CasStrategyOptions) {
+    super();
+  }
+
+  authenticate(req: Request, options: any): void {
+    const ticket = req.query.ticket as string;
+    if (ticket) {
+      this.validateTicket(req, ticket);
+    } else {
+      this.redirectToSso(req);
+    }
+  }
+
+  /**
+   * We need to send the user to the SSO endpoint to get a Service Ticket
+   * @param req request parameter
+   */
+  private redirectToSso(req: Request) {
+    const serviceUrl = new URL(
+      this.options.ssoLoginPath,
+      this.options.ssoBaseURL,
+    );
+    serviceUrl.searchParams.set("service", this.service(req, this.options));
+
+    this.redirect(serviceUrl.href);
+  }
+
+  /**
+   * The user has returned from the SSO endpoint with a ticket.
+   * We need to validate the ticket, and then log the user in.
+   * @param req request object
+   * @param ticket SSO ticket that we need to validate.
+   */
+  private async validateTicket(req: Request, ticket: string) {
+    const validationUrl = new URL(
+      this.options.ssoValidationPath,
+      this.options.ssoBaseURL,
+    );
+
+    validationUrl.searchParams.set("service", this.service(req, this.options));
+    validationUrl.searchParams.set("ticket", ticket);
+
+    try {
+      const result = await got.get(validationUrl, { responseType: "text" });
+
+      const parsedJsObject = this.xmlToJs(result.body);
+
+      // This is an unexpected error. There should always be a response from the CAS server.
+      if (!parsedJsObject["cas:serviceResponse"])
+        return this.error(
+          new Error(`Unexpected response from CAS server: ${result.body}`),
+        );
+
+      const responseContent = parsedJsObject["cas:serviceResponse"] as Record<
+        string,
+        unknown
+      >;
+
+      // This is a somewhat expected error: there may be cases where the ticket fails. Give a good error before failing.
+      if (responseContent.hasOwnProperty("cas:authenticationFailure")) {
+        Logger.warn(
+          `Failed this authentication: ${responseContent["cas:authenticationFailure"]}`,
+        );
+        return this.fail("Ticket validation failed", 401);
+      } else if (responseContent.hasOwnProperty("cas:authenticationSuccess")) {
+        // Expected success flow
+        const body = responseContent["cas:authenticationSuccess"];
+        const user = this.validate(body);
+        return this.success(user);
+      } else {
+        // Unexpected error flow
+        throw new Error(
+          `This is an unexpected error, body is ${responseContent}`,
+        );
+      }
+    } catch (e: any) {
+      Logger.error("The request to the service endpoint failed.");
+      Logger.error(
+        `The service URL was ${this.service(
+          req,
+          this.options,
+        )} and the ticket ${ticket}`,
+      );
+      Logger.error(`The full request URL was ${validationUrl}`);
+      this.error(e);
+    }
+  }
+
+  private xmlToJs(body: string): Record<string, unknown> {
+    const parser = new XMLParser({
+      numberParseOptions: { leadingZeros: false, hex: false },
     });
+    return parser.parse(body);
   }
 
-  validate(body: any): SamlUser {
-    return {
-      firstName: body.attributes.givenname,
-      lastName: body.attributes.surname,
-      email: body.attributes.mail,
-      id: body.attributes.ugentid,
-      institution: Institution.UGENT,
-    };
+  protected service(req: Request, options: CasStrategyOptions) {
+    return options.serverBaseURL;
   }
 
-  service(req: any): string {
-    const configuration = this.configService.getCurrentConfiguration();
-    const allowedCallbacks : string[] = configuration.auth.allowedClientCallbacks;
-    const callbackURL = req.query.callbackUrl || req.params.callbackURL;
-    const baseURL = configuration.auth.cas.serverBaseURL;
-    if (!callbackURL) {
-      return baseURL + `/auth/login/cas/`;
-    }
-    if (allowedCallbacks.indexOf(callbackURL) === -1) {
-      Logger.warn(`callback to URL ${callbackURL} is not allowed`);
-      return baseURL + `/auth/login/cas/`;
-    }
-    const urlEncoded = encodeURIComponent(callbackURL);
-    return baseURL + `/auth/login/cas/${urlEncoded}`;
+  protected validate(body: unknown) {
+    return body;
   }
 }
