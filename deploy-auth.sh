4fc65eb4ab48862d84799d4038ccc46f27f6648f
smessie <smessie@smessie.com>
2022-11-23 23:34:39 +0100

    fix: Load docker-compose override files as well

--- deploy-auth.sh
+++ deploy-auth.sh
@@ -1,2 +1,2 @@
-docker-compose -f authentication-service/blokat-authentication/docker-compose-auth.yml up --detach --build
+docker-compose -f authentication-service/blokat-authentication/docker-compose-auth.yml -f authentication-service/blokat-authentication/docker-compose-auth.override.yml up --detach --build
 docker-compose ps
