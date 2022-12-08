fc739e5872fc908470e11e636378f96698193613
Ewout <ewout.verlinde@ugent.be>
2022-12-08 20:01:51 +0100

    refactor: abstracted the authorization process and fixed some ESLint errors passing by

--- frontend/src/app/services/functionality/application-type/application-type-functionality.service.ts
+++ frontend/src/app/services/functionality/application-type/application-type-functionality.service.ts
@@ -1,77 +1,77 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 import {
-  APPLICATION_TYPE,
-  environment,
+    APPLICATION_TYPE,
+    environment,
 } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ApplicationTypeFunctionalityService {
-  showPenaltyFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return true;
-      default:
-        return true;
+    showPenaltyFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return true;
+            default:
+                return true;
+        }
     }
-  }
 
-  showScanningFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return true;
-      default:
-        return true;
+    showScanningFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return true;
+            default:
+                return true;
+        }
     }
-  }
 
-  showReservationsFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return true;
-      default:
-        return true;
+    showReservationsFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return true;
+            default:
+                return true;
+        }
     }
-  }
 
-  showLockersManagementFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return false;
-      default:
-        return true;
+    showLockersManagementFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return false;
+            default:
+                return true;
+        }
     }
-  }
 
-  showProfilePersonalCalendarFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return false;
-      default:
-        return true;
+    showProfilePersonalCalendarFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return false;
+            default:
+                return true;
+        }
     }
-  }
 
-  showChangePasswordFunctionality(): boolean {
-    switch (environment.applicationType) {
-      case APPLICATION_TYPE.BLOK_AT:
-        return true;
-      case APPLICATION_TYPE.MINI_THERMIS:
-        return false;
-      default:
-        return true;
+    showChangePasswordFunctionality(): boolean {
+        switch (environment.applicationType) {
+            case APPLICATION_TYPE.BLOK_AT:
+                return true;
+            case APPLICATION_TYPE.MINI_THERMIS:
+                return false;
+            default:
+                return true;
+        }
     }
-  }
 }
