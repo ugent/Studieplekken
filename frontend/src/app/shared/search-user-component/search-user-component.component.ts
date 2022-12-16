b2dbf0ccd9d7aace15379e5a2a7795391a669fe4
Ewout <ewout.verlinde@ugent.be>
2022-12-16 15:54:45 +0100

    fix: volunteer management panel layout

--- frontend/src/app/shared/search-user-component/search-user-component.component.ts
+++ frontend/src/app/shared/search-user-component/search-user-component.component.ts
@@ -1,33 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { TableDataService } from 'src/app/stad-gent-components/atoms/table/data-service/table-data-service.service';
-import { User } from '../model/User';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {TableDataService} from 'src/app/stad-gent-components/atoms/table/data-service/table-data-service.service';
+import {User} from '../model/User';
+import {TabularData} from "../../stad-gent-components/atoms/table/tabular-data";
 
 @Component({
-  selector: 'app-search-user-component',
-  templateUrl: './search-user-component.component.html',
-  styleUrls: ['./search-user-component.component.scss']
+    selector: 'app-search-user-component',
+    templateUrl: './search-user-component.component.html',
+    styleUrls: ['./search-user-component.component.scss']
 })
 export class SearchUserComponentComponent implements OnInit {
 
-  @Input() icon: string = "icon-hamburger";
-  @Output() selectedUser = new EventEmitter<User>();
-  users: User[];
+    @Input() icon = 'icon-hamburger';
+    @Output() selectedUser = new EventEmitter<User>();
+    users: User[];
 
+    constructor(private table: TableDataService) {}
 
-  constructor(private table: TableDataService) { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    newUsers(users: User[]): void {
+        this.users = users;
+    }
 
-  newUsers(users: User[]) {
-    this.users = users;
-  }
+    outputUser(user: User): void {
+        this.selectedUser.next(user);
+    }
 
-  outputUser(user: User) {
-    this.selectedUser.next(user);
-  }
-
-  getTableData(users: User[]) {
-    return this.table.usersToTable(users, this.icon)
-  }
+    getTableData(users: User[]): TabularData<User>  {
+        return this.table.usersToTable(users, this.icon);
+    }
 }
