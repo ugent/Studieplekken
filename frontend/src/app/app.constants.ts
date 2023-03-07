c9fd692d3fc892b7a93ec33b9e63c60df4387bf9
ewout <ewout.verlinde@ugent.be>
2023-03-07 09:53:08 +0100

    style: added representative colors per HOI

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -8,32 +8,6 @@ export const authenticationWasExpiredUrlLSKey = 'authenticationWasExpiredUrl'
  */
 export const msToShowFeedback = 10000;
 
-/*
- * The PenaltyEvent code for a manual entry
- */
-export const penaltyEventCodeForManualEntry = 16663;
-
-/*
- * The roles that are available in the application.
- * This is used for being able to manage the role(s)
- * of a certain user.
- *
- * Do not forget to translate the role if you would
- * be adding a role (<lang>.json -> general.roles)
- */
-export const rolesArray = ['ADMIN'];
-
-/*
- * The possible statuses of a location.
- */
-
-export const locationStatusArray = [
-  'OPEN',
-  'CLOSED',
-  'CLOSED_UPCOMING',
-  'CLOSED_ACTIVE',
-];
-
 export enum LocationStatus {
   OPEN = 'OPEN',
   CLOSED = 'CLOSED',
@@ -41,15 +15,18 @@ export enum LocationStatus {
   CLOSED_ACTIVE = 'CLOSED_ACTIVE',
 }
 
-/*
- * This variable maps all the supported languages to its
- * database representation (LANGUAGES.enum)
- */
-export const languageAsEnum = {
-  nl: 'DUTCH',
-  en: 'ENGLISH',
-};
-
 export const defaultOpeningHour = 8;
 
 export const defaultClosingHour = 17;
+
+/**
+ * Representative colors per institution.
+ */
+export const HOIColors = {
+    'HoGent': '#333',
+    'UGent': '#1E64C8',
+    'Arteveldehogeschool': '#f58732',
+    'KULeuven': '#6ac2ee',
+    'Odisee': '#1f416b',
+    'Luca': '#feb3d2'
+};
--- frontend/src/app/management/stats/chart/chart.component.ts
+++ frontend/src/app/management/stats/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {Chart} from "chart.js/auto";
+import {HOIColors} from "../../../app.constants";
 
 @Component({
     selector: 'app-chart',
@@ -26,14 +27,12 @@ export class ChartComponent implements AfterViewInit {
      * We use this hook to make sure the canvas was loaded.
      */
     ngAfterViewInit(): void {
-        let color = '#b2d4e2';
         let datasets = {};
 
         for (let hois of Object.values(this.reservations)) {
             for (let [hoi, reservations] of Object.entries(hois)) {
                 if (!datasets[hoi]) {
-                    datasets[hoi] = { label: hoi, data: [], backgroundColor: color };
-                    color = this.decreaseTint(color, 25);
+                    datasets[hoi] = { label: hoi, data: [], backgroundColor: HOIColors[hoi] ?? '#1E64C8' };
                 }
                 datasets[hoi].data.push(reservations);
             }
@@ -56,27 +55,5 @@ export class ChartComponent implements AfterViewInit {
                 }
             }
         });
-    }
-
-    /**
-     * Decrease a hex color in brightness.
-     *
-     * @param hex
-     * @param amount
-     * @private
-     */
-    private decreaseTint(hex: string, amount: number): string {
-        // parse the hex color string to RGB values
-        let r = parseInt(hex.slice(1, 3), 16);
-        let g = parseInt(hex.slice(3, 5), 16);
-        let b = parseInt(hex.slice(5, 7), 16);
-
-        // decrease the RGB values by the specified amount
-        r = Math.max(0, r - amount);
-        g = Math.max(0, g - amount);
-        b = Math.max(0, b - amount);
-
-        // convert the RGB values back to hex color string
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
 }
