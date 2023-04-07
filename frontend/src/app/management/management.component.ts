a86478a487f94daf625b0f92774fde4009b6fa93
ewout <ewout.verlinde@ugent.be>
2023-04-07 20:49:42 +0200

    fix: admin management navigation

--- frontend/src/app/management/management.component.ts
+++ frontend/src/app/management/management.component.ts
@@ -1,59 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ApplicationTypeFunctionalityService } from '../services/functionality/application-type/application-type-functionality.service';
-import { AuthenticationService } from '../services/authentication/authentication.service';
-import { combineLatest } from 'rxjs';
-import { BreadcrumbService, managementBreadcrumb } from '../stad-gent-components/header/breadcrumbs/breadcrumb.service';
-import { LoginRedirectService } from '../services/authentication/login-redirect.service';
-import { environment } from 'src/environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {
+    ApplicationTypeFunctionalityService
+} from '../services/functionality/application-type/application-type-functionality.service';
+import {AuthenticationService} from '../services/authentication/authentication.service';
+import {combineLatest} from 'rxjs';
+import {BreadcrumbService, managementBreadcrumb} from '../stad-gent-components/header/breadcrumbs/breadcrumb.service';
+import {LoginRedirectService} from '../services/authentication/login-redirect.service';
+import {environment} from 'src/environments/environment';
 
 @Component({
-  selector: 'app-management',
-  templateUrl: './management.component.html',
-  styleUrls: ['./management.component.scss'],
+    selector: 'app-management',
+    templateUrl: './management.component.html',
+    styleUrls: ['./management.component.scss'],
 })
 export class ManagementComponent implements OnInit {
-  showReservations: boolean;
-  showPenalties: boolean;
-  showTagManagement: boolean;
-  showAdmin: boolean = this.authenticationService.isAdmin();
-  showVolunteersManagement: boolean;
-  showActionlog: boolean;
-  showStats: boolean;
-  showStagingWarning = environment.showStagingWarning;
-  isMobile: boolean;
-  MOBILE_SIZE = 500;
+    showTagManagement: boolean;
+    showAdmin: boolean;
+    showVolunteersManagement: boolean;
+    showActionlog: boolean;
+    showStats: boolean;
+    showStagingWarning = environment.showStagingWarning;
 
+    constructor(
+        private authenticationService: AuthenticationService,
+        private breadcrumbsService: BreadcrumbService
+    ) {}
 
-  constructor(
-    private functionalityService: ApplicationTypeFunctionalityService,
-    private authenticationService: AuthenticationService,
-    private breadcrumbsService: BreadcrumbService
-  ) {
-  }
+    ngOnInit(): void {
+        // Show certain functionality depending on the role of the user
+        const authenticatedUserObs = this.authenticationService.user;
+        const hasAuthoritiesObs = this.authenticationService.hasAuthoritiesObs;
 
-  ngOnInit(): void {
-    // Show certain functionality depending on type of application
-    this.showReservations = this.functionalityService.showReservationsFunctionality();
-    this.showPenalties = this.functionalityService.showPenaltyFunctionality();
+        combineLatest([authenticatedUserObs, hasAuthoritiesObs]).subscribe(
+            ([authenticatedUser, hasAuthorities]) => {
+                this.showAdmin = authenticatedUser.admin;
+                this.showTagManagement = authenticatedUser.admin;
+                this.showVolunteersManagement = authenticatedUser.admin || hasAuthorities;
+                this.showActionlog = authenticatedUser.admin;
+                this.showStats = authenticatedUser.admin;
+            }
+        );
 
-    // Show certain functionality depending on the role of the user
-    const authenticatedUserObs = this.authenticationService.user;
-    const hasAuthoritiesObs = this.authenticationService.hasAuthoritiesObs;
-
-    combineLatest([authenticatedUserObs, hasAuthoritiesObs]).subscribe(
-      ([authenticatedUser, hasAuthorities]) => {
-            this.showTagManagement = authenticatedUser.admin;
-            this.showVolunteersManagement = authenticatedUser.admin || hasAuthorities;
-            this.showActionlog = authenticatedUser.admin;
-            this.showStats = authenticatedUser.admin;
-        }
-    );
-
-    this.breadcrumbsService.setCurrentBreadcrumbs([managementBreadcrumb]);
-    this.isMobile = window.innerWidth < this.MOBILE_SIZE;
-  }
-
-  getSize(): string {
-    return this.isMobile ? '80%' : '23%';
-  }
+        this.breadcrumbsService.setCurrentBreadcrumbs([managementBreadcrumb]);
+    }
 }
