e12a5716dd8cfb8de28c757165c20310ab26a37c
EwoutV <ewoutverlinde@hotmail.com>
2024-05-09 17:03:23 +0200

    fix: reservation update

--- frontend/src/app/components/dashboard/location-reservation/location-reservation.component.ts
+++ frontend/src/app/components/dashboard/location-reservation/location-reservation.component.ts
@@ -18,6 +18,7 @@ import {
 import {tap} from 'rxjs/operators';
 import {ModalComponent} from '../../stad-gent-components/molecules/modal/modal.component';
 import {AfterReservationComponent} from './after-reservation/after-reservation.component';
+import {Authority} from '@/model/Authority';
 import {Authority} from '../../../model/Authority';
 import {now} from 'd3';
 
@@ -74,9 +75,12 @@ export class LocationReservationComponent implements OnInit, OnDestroy {
             this.locationService.getLocation(locationID)
         ]).pipe(
             tap(([user, location]) => {
+
                 this.location = location;
                 this.user = user;
+
                 this.isNotFound = !!!location;
+
                 this.showEdit = user.isAdmin() || user.userAuthorities.some((authority: Authority) =>
                     authority.authorityId === location.authority.authorityId
                 );
@@ -85,11 +89,14 @@ export class LocationReservationComponent implements OnInit, OnDestroy {
                     url: `/dashboard/${location?.locationId}`
                 }]);
 
-                this.updateEvents().then(() => this.updateReservations());
+                this.updateReservations();
             })
         );
     }
 
+    /**
+     * Set up the subscriptions for the component.
+     */
     setupSubscriptions(): void {
         // Update reservations (and calendar) every minute.
         this.subscription.add(
@@ -106,112 +113,136 @@ export class LocationReservationComponent implements OnInit, OnDestroy {
         );
     }
 
+    /**
+     * Update the reservations list with data from the backend.
+     */
     updateReservations(reset = false): void {
-        if (this.location) {
-            this.authenticationService.getLocationReservations().subscribe((reservations) => {
-                // Update the reservations list and sort it.
-                this.allReservations = reservations.filter(reservation =>
-                    reservation.timeslot.locationId === this.location.locationId
-                ).sort((a, b) =>
-                    Number(b.timeslot.getStartMoment()) - Number(a.timeslot.getStartMoment())
-                );
-
-                if (reset) {
-                    this.removedReservations = [];
-                    this.newReservations = [];
-                }
-            });
+        if (!this.location) {
+            return;
         }
-    }
 
-    async updateEvents(fetchTimeslots = true): Promise<void> {
-        if (this.location) {
-            // Fetch and reload the timeslots from the backend.
-            if (fetchTimeslots) {
-                this.timeslots = await this.timeslotsService.getTimeslotsOfLocation(this.location.locationId).pipe(
-                    tap((timeslots: Timeslot[]) =>
-                        this.isReservable = timeslots.some(timeslot =>
-                            timeslot.reservableFrom?.isSameOrBefore(
-                                moment()
-                            ) && !timeslot.isInPast()
-                        )
-                    )
-                ).toPromise();
+        this.authenticationService.getLocationReservations().subscribe((reservations) => {
+            // Update the reservations list and sort it.
+            this.allReservations = reservations.filter(reservation =>
+                reservation.timeslot.locationId === this.location.locationId
+            ).sort((a, b) =>
+                Number(b.timeslot.getStartMoment()) - Number(a.timeslot.getStartMoment())
+            );
+
+            // Reset the new and removed reservations if requested.
+            if (reset) {
+                this.removedReservations = [];
+                this.newReservations = [];
             }
+        });
 
-            // Convert the timeslots into calendar events.
-            this.events = this.timeslots.map(timeslot => this.timeslotCalendarEventService.timeslotToCalendarEvent(
-                timeslot, this.language, [
-                    ...this.newReservations, ...this.allReservations.filter(res =>
-                        !this.removedReservations.some(res2 =>
-                            timeslotEquals(res.timeslot, res2.timeslot)
-                        )
-                    )
-                ]
-            ));
-        }
+        // Update the calendar events.
+        void this.updateEvents();
     }
 
+    /**
+     * Update the events list with data from the backend.
+     */
+    async updateEvents(fetchTimeslots = true): Promise<void> {
+        if (!this.location) {
+            return;
+        }
+
+        // Fetch and reload the timeslots from the backend.
+        if (fetchTimeslots) {
+            this.timeslots = await this.timeslotsService.getTimeslotsOfLocation(this.location.locationId).pipe(
+                tap((timeslots: Timeslot[]) =>
+                    this.isReservable = timeslots.some(timeslot =>
+                        timeslot.reservableFrom?.isSameOrBefore(
+                            moment()
+                        ) && !timeslot.isInPast()
+                    )
+                )
+            ).toPromise();
+        }
+
+        // Convert the timeslots into calendar events.
+        this.events = this.timeslots.map(timeslot => this.timeslotCalendarEventService.timeslotToCalendarEvent(
+            timeslot, this.language, [
+                ...this.newReservations, ...this.allReservations.filter(res =>
+                    !this.removedReservations.some(res2 =>
+                        timeslotEquals(res.timeslot, res2.timeslot)
+                    )
+                )
+            ]
+        ));
+    }
+
+    /**
+     * Select a timeslot.
+     *
+     * @param event The event containing the selected timeslot.
+     */
     timeslotPicked(event: { timeslot: Timeslot }): void {
         const currentUser = this.user;
 
-        if (currentUser.isLoggedIn()) {
-            const currentTimeslot: Timeslot = event.timeslot;
+        if (!currentUser.isLoggedIn()) {
+            return;
+        }
 
-            // Only logged-in users can select timeslots.
-            if (!currentUser.userId || !currentTimeslot || !currentTimeslot.reservableFrom) {
-                return;
-            }
+        const currentTimeslot: Timeslot = event.timeslot;
 
-            // The timeslot should be in the future.
-            if (moment().isBefore(currentTimeslot.reservableFrom) || currentTimeslot.isInPast()) {
-                return;
-            }
+        // Only logged-in users can select timeslots.
+        if (!currentUser.userId || !currentTimeslot || !currentTimeslot.reservableFrom) {
+            return;
+        }
 
-            const currentReservation: LocationReservation = new LocationReservation(
-                currentUser, currentTimeslot
+        // The timeslot should be in the future.
+        if (moment().isBefore(currentTimeslot.reservableFrom) || currentTimeslot.isInPast()) {
+            return;
+        }
+
+        const currentReservation: LocationReservation = new LocationReservation(
+            currentUser, currentTimeslot
+        );
+
+        const selected = [...this.allReservations, ...this.newReservations].some((other: LocationReservation) =>
+            timeslotEquals(currentReservation.timeslot, other.timeslot) &&
+            !other.isCanceled() && !other.timeslot.isInPast() &&
+            !this.removedReservations.some(other1 =>
+                timeslotEquals(other1.timeslot, other.timeslot)
+            )
+        );
+
+        // Only continue if there are free seats or when the slot already was selected.
+        if (currentTimeslot.amountOfReservations < currentTimeslot.seatCount || selected) {
+            // Try to find an existing reservation for this timeslot.
+            const oldReservation = this.allReservations.find(
+                reservation => reservation.timeslot.timeslotSequenceNumber === currentTimeslot.timeslotSequenceNumber
             );
 
-            const selected = [...this.allReservations, ...this.newReservations].some((other: LocationReservation) =>
-                timeslotEquals(currentReservation.timeslot, other.timeslot) &&
-                !other.isCanceled() && !other.timeslot.isInPast() &&
-                !this.removedReservations.some(other1 =>
-                    timeslotEquals(other1.timeslot, other.timeslot)
-                )
-            );
-
-            // Only continue if there are free seats or when the slot already was selected.
-            if (currentTimeslot.amountOfReservations < currentTimeslot.seatCount || selected) {
-                // Try to find an existing reservation for this timeslot.
-                const oldReservation = this.allReservations.find(
-                    reservation => reservation.timeslot.timeslotSequenceNumber === currentTimeslot.timeslotSequenceNumber
-                );
-
-                if (!selected || !oldReservation?.isCommitted()) {
-                    if (selected) {
-                        // Deselect already selected timeslots.
-                        if (oldReservation && !oldReservation.isCanceled()) {
-                            this.removedReservations.push(oldReservation);
-                        }
-                        this.newReservations = this.newReservations.filter(reservation =>
-                            !timeslotEquals(reservation.timeslot, currentReservation.timeslot)
-                        );
-                    } else {
-                        // Select unselected timeslots.
-                        if (!oldReservation || oldReservation.isCanceled()) {
-                            this.newReservations.push(currentReservation);
-                        }
-                        this.removedReservations = this.removedReservations.filter(reservation =>
-                            !timeslotEquals(reservation.timeslot, currentReservation.timeslot)
-                        );
+            if (!selected || !oldReservation?.isCommitted()) {
+                if (selected) {
+                    // Deselect already selected timeslots.
+                    if (oldReservation && !oldReservation.isCanceled()) {
+                        this.removedReservations.push(oldReservation);
                     }
+                    this.newReservations = this.newReservations.filter(reservation =>
+                        !timeslotEquals(reservation.timeslot, currentReservation.timeslot)
+                    );
+                } else {
+                    // Select unselected timeslots.
+                    if (!oldReservation || oldReservation.isCanceled()) {
+                        this.newReservations.push(currentReservation);
+                    }
+                    this.removedReservations = this.removedReservations.filter(reservation =>
+                        !timeslotEquals(reservation.timeslot, currentReservation.timeslot)
+                    );
                 }
-
-                void this.updateEvents(false);
             }
+
+            void this.updateEvents(false);
         }
     }
 
+    /**
+     * Destroy the component and its subscriptions.
+     */
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
     }
