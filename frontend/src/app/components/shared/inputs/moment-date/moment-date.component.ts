b39cd22bb5afa962f11c6d4137491aa16f2f08b2
Ewout <ewoutverlinde@hotmail.com>
2024-12-22 23:38:34 +0100

    fix: time selector component resets date/time upon time/date selection

--- frontend/src/app/components/shared/inputs/moment-date/moment-date.component.ts
+++ frontend/src/app/components/shared/inputs/moment-date/moment-date.component.ts
@@ -19,35 +19,71 @@ export class MomentDateComponent implements ControlValueAccessor {
     @Input() min: Moment;
     @Input() max: Moment;
 
-    protected dateOrTimeValue: Moment;
+    protected dateOrTimeValue: Moment = moment();
     protected isDisabled: boolean = false;
     protected onDateOrTimeChange: (date: Moment) => void;
     protected onDateOrTimeTouched: () => void;
 
+    /**
+     * @inheritdoc
+     */
     public writeValue(dateOrTime: Moment): void {
         this.dateOrTimeValue = dateOrTime;
     }
 
+    /**
+     * @inheritdoc
+     */
     public registerOnChange(onChange: (dateOrTime: Moment) => void): void {
         this.onDateOrTimeChange = onChange;
     }
 
+    /**
+     * @inheritdoc
+     */
     public registerOnTouched(onTouched: () => void): void {
         this.onDateOrTimeTouched = onTouched;
     }
 
+    /**
+     * @inheritdoc
+     */
     public setDisabledState(isDisabled: boolean): void {
         this.isDisabled = isDisabled;
     }
 
+    /**
+     * Handles the input change event for date or time input fields.
+     *
+     * @param event - The input change event.
+     * @param part - Specifies whether the input is for 'date' or 'time'.
+     * 
+     * This method parses the input value based on the specified part ('date' or 'time')
+     * and updates the `dateOrTimeValue` property accordingly. If a callback function
+     * `onDateOrTimeChange` is provided, it will be called with the updated value.
+     */
     public handleInputChange(event: Event, part: 'date' | 'time'): void {
         const input = event.target as HTMLInputElement;
         const value = input.value;
 
+        if (!this.dateOrTimeValue) {
+            this.dateOrTimeValue = moment();
+        }
+
         if (part === 'date') {
-            this.dateOrTimeValue = moment(value, 'YYYY-MM-DD');
+            const newDate = moment(value, 'YYYY-MM-DD');
+
+            if (newDate.isValid()) {
+                this.dateOrTimeValue.date(newDate.date());
+            }
         } else if (part === 'time') {
-            this.dateOrTimeValue = moment(value, 'HH:mm');
+            const newTime = moment(value, 'HH:mm');
+
+            if (newTime.isValid()) {
+                this.dateOrTimeValue
+                    .hour(newTime.hour())
+                    .minute(newTime.minute());
+            }
         }
 
         if (this.onDateOrTimeChange) {
@@ -55,6 +91,13 @@ export class MomentDateComponent implements ControlValueAccessor {
         }
     }
 
+    /**
+     * Formats a given Moment object into a string based on the provided format.
+     *
+     * @param moment - The Moment object to format. If null or undefined, the function will not return a formatted string.
+     * @param format - The string format to apply to the Moment object.
+     * @returns The formatted date string if the Moment object is valid; otherwise, undefined.
+     */
     public getFormattedValue(moment: Moment, format: string): string {
         if (moment !== null && moment !== undefined) {
             return moment.format(format);
