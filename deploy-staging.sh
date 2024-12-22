abb5f1cf4291af15b7b8186748af77ea76a5978d
Ewout <ewoutverlinde@hotmail.com>
2024-12-22 23:11:20 +0100

    fix: deploy scripts docker-compose depcrecation

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,5 +2,5 @@
 
 ./scripts/database_backup.sh "deploy"
 
-docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-staging.yml -f docker-compose-staging.override.yml up --detach --build
-docker-compose ps
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-staging.yml -f docker-compose-staging.override.yml up --detach --build
+docker compose ps
